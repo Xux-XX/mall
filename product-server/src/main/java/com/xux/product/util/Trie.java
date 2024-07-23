d9d53529a356939b7d5e1e651fd6e10c6e361650
Xux <2166902048@qq.com>
2024-07-23 17:54:58 +0800

    修改trie为读写锁

--- product-server/src/main/java/com/xux/product/util/Trie.java
+++ product-server/src/main/java/com/xux/product/util/Trie.java
@@ -6,6 +6,9 @@ import org.springframework.data.util.Pair;
 import org.springframework.stereotype.Component;
 
 import java.util.*;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
  * 使用AC自动机快速匹配敏感词
@@ -15,6 +18,7 @@ import java.util.*;
  */
 @Component
 public class Trie {
+    private final ReadWriteLock lock = new ReentrantReadWriteLock();
 
     /**
      * 将输入文本中的敏感词替换为目标字符串
@@ -23,35 +27,42 @@ public class Trie {
      * @return 替换后文本
      */
     public String replaceWith(String text, String target) {
-        // 用List记录匹配到的敏感词的首位索引 Pair(begin, end)
-        List<Pair<Integer, Integer>> indexList = new ArrayList<>();
-        Node now = root;
-        for(int i = 0; i < text.length(); i++){
-            char ch = text.charAt(i);
-            // 匹配失败则通过fail指针跳转到下一个状态
-            if (!now.next.containsKey(ch)){
-                now = now.fail;
-                continue;
+        Lock readLock = lock.readLock();
+        readLock.lock();
+        try {
+            // 用List记录匹配到的敏感词的首位索引 Pair(begin, end)
+            List<Pair<Integer, Integer>> indexList = new ArrayList<>();
+            Node now = root;
+            for(int i = 0; i < text.length(); i++){
+                char ch = text.charAt(i);
+                // 匹配失败则通过fail指针跳转到下一个状态
+                if (!now.next.containsKey(ch)){
+                    now = now.fail;
+                    continue;
+                }
+                // 匹配成功,进入下一个状态
+                now = now.next.get(ch);
+                if (now.length != 0){
+                    // 匹配成功一个串后将now置为root避免交叉匹配
+                    indexList.add(Pair.of(i - now.length + 1, i));
+                    now = root;
+                }
             }
-            // 匹配成功,进入下一个状态
-            now = now.next.get(ch);
-            if (now.length != 0){
-                // 匹配成功一个串后将now置为root避免交叉匹配
-                indexList.add(Pair.of(i - now.length + 1, i));
-                now = root;
+            // 构造需要返回的字符串
+            StringBuilder stringBuilder = new StringBuilder();
+            int left = 0;
+            for (Pair<Integer, Integer> pair : indexList) {
+                String substring = text.substring(left, pair.getFirst());
+                stringBuilder.append(substring);
+                stringBuilder.append(target);
+                left = pair.getSecond() + 1;
             }
+            stringBuilder.append(text.substring(left));
+            return stringBuilder.toString();
         }
-        // 构造需要返回的字符串
-        StringBuilder stringBuilder = new StringBuilder();
-        int left = 0;
-        for (Pair<Integer, Integer> pair : indexList) {
-            String substring = text.substring(left, pair.getFirst());
-            stringBuilder.append(substring);
-            stringBuilder.append(target);
-            left = pair.getSecond() + 1;
+        finally {
+            readLock.unlock();
         }
-        stringBuilder.append(text.substring(left));
-        return stringBuilder.toString();
     }
 
     /**
@@ -70,18 +81,25 @@ public class Trie {
      *      <p>false 不包含敏感词
      */
     public boolean hasSensitive(String text){
-        Node now = root;
-        for (int i = 0; i < text.length(); i++) {
-            char ch = text.charAt(i);
-            if (!now.next.containsKey(ch)){
-                now = now.fail;
-                continue;
+        Lock readLock = lock.readLock();
+        readLock.lock();
+        try {
+            Node now = root;
+            for (int i = 0; i < text.length(); i++) {
+                char ch = text.charAt(i);
+                if (!now.next.containsKey(ch)) {
+                    now = now.fail;
+                    continue;
+                }
+                // 匹配成功进入下一个状态
+                now = now.next.get(ch);
+                if (now.length != 0) return true;
             }
-            // 匹配成功进入下一个状态
-            now = now.next.get(ch);
-            if (now.length != 0) return true;
+            return false;
         }
-        return false;
+        finally {
+            readLock.unlock();
+        }
     }
 
     /**
@@ -89,33 +107,40 @@ public class Trie {
      * @param word 敏感词
      */
     public synchronized void addWord(String word) {
-        // trie数新增节点
-        Node now = root;
-        for (int i = 0; i < word.length(); i++){
-            char ch = word.charAt(i);
-            Node node = now.next.get(ch);
-            if (node == null){
-                // 如果是新建节点, 需要更新一下周围的fail指针
-                node = new Node(ch);
-                List<Node> nodeList = reverseList.computeIfAbsent(now, n -> new ArrayList<>());
-                // 遍历fail指针为"新节点的父节点"的节点
-                // 若子节点存在值为ch的节点,将这个节点的fail指针指向新节点
-                for (Node node1 : nodeList) {
-                    if (node1.next.containsKey(ch)){
-                        Node child = node1.next.get(ch);
-                        reverseList.get(child.fail).remove(child);
-                        child.fail = node;
-                        reverseList.computeIfAbsent(node, n -> new ArrayList<>()).add(child);
+        Lock writeLock = lock.writeLock();
+        writeLock.lock();
+        try {
+            // trie数新增节点
+            Node now = root;
+            for (int i = 0; i < word.length(); i++){
+                char ch = word.charAt(i);
+                Node node = now.next.get(ch);
+                if (node == null){
+                    // 如果是新建节点, 需要更新一下周围的fail指针
+                    node = new Node(ch);
+                    List<Node> nodeList = reverseList.computeIfAbsent(now, n -> new ArrayList<>());
+                    // 遍历fail指针为"新节点的父节点"的节点
+                    // 若子节点存在值为ch的节点,将这个节点的fail指针指向新节点
+                    for (Node node1 : nodeList) {
+                        if (node1.next.containsKey(ch)){
+                            Node child = node1.next.get(ch);
+                            reverseList.get(child.fail).remove(child);
+                            child.fail = node;
+                            reverseList.computeIfAbsent(node, n -> new ArrayList<>()).add(child);
+                        }
                     }
+                    // 更新这个新建节点的fail指针,(需要特判父节点为root的情况)
+                    if(now != root)node.fail = now.fail.next.getOrDefault(ch, root);
+                    else node.fail = root;
+                    now.next.put(ch, node);
                 }
-                // 更新这个新建节点的fail指针,(需要特判父节点为root的情况)
-                if(now != root)node.fail = now.fail.next.getOrDefault(ch, root);
-                else node.fail = root;
-                now.next.put(ch, node);
+                now = node;
             }
-            now = node;
+            now.length = word.length();
         }
-        now.length = word.length();
+        finally {
+            writeLock.unlock();
+        }
     }
 
 
@@ -134,29 +159,35 @@ public class Trie {
      * @param now 当前的节点
      */
     private void removeWordRecurse(String word, int index, Node now){
-        if (index == word.length()){
-            // 匹配成功, 将length标记置为0表示删除这个单词
-            now.length = 0;
-            return;
-        }
-        char ch = word.charAt(index);
-        Node child = now.next.get(ch);
-        if (child == null) return;
-        // 匹配成功, 递归下一个单词
-        removeWordRecurse(word, index + 1, child);
-        if (child.next.isEmpty()){
-            // 若子节点没有后继节点需要将子节点删除, 并更新fail指针
-            now.next.remove(ch);
-            List<Node> nodeList = reverseList.get(child);
-            if (nodeList != null){
-                reverseList.remove(child);
-                for (Node node : nodeList) {
-                    reverseList.get(child.fail).add(node);
-                    node.fail = child.fail;
+        Lock writeLock = lock.writeLock();
+        writeLock.unlock();
+        try {
+            if (index == word.length()){
+                // 匹配成功, 将length标记置为0表示删除这个单词
+                now.length = 0;
+                return;
+            }
+            char ch = word.charAt(index);
+            Node child = now.next.get(ch);
+            if (child == null) return;
+            // 匹配成功, 递归下一个单词
+            removeWordRecurse(word, index + 1, child);
+            if (child.next.isEmpty()){
+                // 若子节点没有后继节点需要将子节点删除, 并更新fail指针
+                now.next.remove(ch);
+                List<Node> nodeList = reverseList.get(child);
+                if (nodeList != null){
+                    reverseList.remove(child);
+                    for (Node node : nodeList) {
+                        reverseList.get(child.fail).add(node);
+                        node.fail = child.fail;
+                    }
                 }
             }
         }
-
+        finally {
+            writeLock.unlock();
+        }
     }
 
     /**
@@ -165,12 +196,10 @@ public class Trie {
     @PostConstruct
     public void load() {
         // 建立字典树
-        int cnt = 0;
         for (String s : mapper.selectWordList()) {
             Node now = root;
             for (int i=0; i < s.length(); i++){
                 char ch = s.charAt(i);
-                if (!now.next.containsKey(ch)) cnt ++;
                 now = now.next.computeIfAbsent(ch, Node::new);
             }
             now.length = s.length();
